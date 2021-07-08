f4238eed0505085903d59561b36e4de758f0e168
junjungoal <junyamada107@gmail.com>
2021-07-08 12:19:48 +0900

    update

--- scripts/mycobot/slider_control.py
+++ scripts/mycobot/slider_control.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python2
+# from std_msgs.msg import String
 import time
+
 import rospy
 from sensor_msgs.msg import JointState
 
@@ -10,28 +12,30 @@ mc = None
 
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "%s", data)
+    # rospy.loginfo(rospy.get_caller_id() + "%s", data.position)
+    print(data.position)
     data_list = []
     for index, value in enumerate(data.position):
-        # if index != 2:
-        #     value *= -1
         data_list.append(value)
 
     mc.send_radians(data_list, 80)
-    
+    # time.sleep(0.5)
+
+
 def listener():
     global mc
-    rospy.init_node('mycobot_reciver', anonymous=True)
+    rospy.init_node("control_slider", anonymous=True)
 
-    port = rospy.get_param('~port', '/dev/ttyUSB0')
-    baud = rospy.get_param('~baud', 115200)
+    rospy.Subscriber("joint_states", JointState, callback)
+    port = rospy.get_param("~port", "/dev/ttyUSB0")
+    baud = rospy.get_param("~baud", 115200)
     print(port, baud)
     mc = MyCobot(port, baud)
 
-    rospy.Subscriber("joint_states", JointState, callback)
-
     # spin() simply keeps python from exiting until this node is stopped
+    print("spin ...")
     rospy.spin()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     listener()
--- scripts/mycobot_moveit/sync_plan.py
+++ scripts/mycobot_moveit/sync_plan.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python2
-# from std_msgs.msg import String
 import time
-import subprocess
-
 import rospy
 from sensor_msgs.msg import JointState
 
@@ -13,30 +10,30 @@ mc = None
 
 
 def callback(data):
-    #rospy.loginfo(rospy.get_caller_id() + "%s", data.position)
-    print(data.position)
+    rospy.loginfo(rospy.get_caller_id() + "%s", data)
     data_list = []
     for index, value in enumerate(data.position):
+        # if index != 2:
+        #     value *= -1
         data_list.append(value)
 
     mc.send_radians(data_list, 80)
-    # time.sleep(0.5)
 
 
 def listener():
     global mc
-    rospy.init_node('control_slider', anonymous=True)
+    rospy.init_node("mycobot_reciver", anonymous=True)
 
-    rospy.Subscriber("joint_states", JointState, callback)
-    port = rospy.get_param('~port', '/dev/ttyUSB0')
-    baud = rospy.get_param('~baud', 115200)
+    port = rospy.get_param("~port", "/dev/ttyUSB0")
+    baud = rospy.get_param("~baud", 115200)
     print(port, baud)
     mc = MyCobot(port, baud)
 
+    rospy.Subscriber("joint_states", JointState, callback)
+
     # spin() simply keeps python from exiting until this node is stopped
-    print('sping ...')
     rospy.spin()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     listener()
